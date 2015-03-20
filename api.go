df8afe41c47ec13e05275daec7651dd5402428b5
Jackson Owens <jackson@squareup.com>
2015-03-20 13:42:20 +0800

    Add API.CurrentSeason() to get season identifier

--- api.go
+++ api.go
@@ -1,6 +1,10 @@
 package nbagame
 
 import (
+	"fmt"
+	"strconv"
+	"time"
+
 	"github.com/jbowens/nbagame/data"
 	"github.com/jbowens/nbagame/endpoints"
 	"github.com/jbowens/nbagame/results"
@@ -8,12 +12,30 @@ import (
 
 var (
 	// API is the default APIClient with default parameters.
-	API = APIClient{&endpoints.DefaultRequester}
+	API = APIClient{&endpoints.DefaultRequester, ""}
 )
 
 // APIClient is the master API object for interating with the API.
 type APIClient struct {
-	Requester *endpoints.Requester
+	Requester     *endpoints.Requester
+	currentSeason string
+}
+
+// CurrentSeason returns the season identifier for the current season. The season switches
+// starting on July 1st.
+func (c *APIClient) CurrentSeason() string {
+	// Cache the current season
+	if c.currentSeason == "" {
+		now := time.Now()
+		year := now.Year()
+		if now.Month() >= time.July {
+			c.currentSeason = fmt.Sprintf("%d-%s", year, strconv.Itoa(year + 1)[2:])
+		} else {
+			c.currentSeason = fmt.Sprintf("%d-%s", year-1, strconv.Itoa(year)[2:])
+		}
+	}
+
+	return c.currentSeason
 }
 
 // AllPlayers returns a slice of all players.
