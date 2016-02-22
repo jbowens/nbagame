74cf1aa0f182b9962e5ffc5084160f20af849509
Jackson Owens <jackson@chain.com>
2016-02-22 01:30:17 -0800

    prefix with turnover

--- data/turnover.go
+++ data/turnover.go
@@ -3,37 +3,37 @@ package data
 // TurnoverType defines the different types of turnovers.
 type TurnoverType int
 
-// TODO(jackson): Fill in the gaps in the table below.
+// TODO(jackson): Fill in the gaps in the turnover table.
 const (
-	UnknownTurnover              TurnoverType = 0
-	BadPassTurnover                           = 1
-	LostBallTurnover                          = 2
-	OutOfBoundsTurnover                       = 3
-	TravelingTurnover                         = 4
-	FoulTurnover                              = 5
-	DoubleDribbleTurnover                     = 6
-	DiscontinueDribbleTurnover                = 7
-	ThreeSecondViolationTurnover              = 8
-	FiveSecondViolationTurnover               = 9
-	EightSecondViolationTurnover              = 10
-	ShotClockTurnover                         = 11
-	InboundTurnover                           = 12
-	BackcourtTurnover                         = 13
-	OffensiveGoaltendingTurnover              = 15
-	LaneViolationTurnover                     = 17
-	JumpBallViolationTurnover                 = 18
-	KickedBallTurnover                        = 19
-	IllegalAssistTurnover                     = 20
-	PalmingTurnover                           = 21
-	PunchedBallTurnover                       = 33
-	SwiningElbowsTurnover                     = 34
-	BasketFromBelowTurnover                   = 35
-	IllegalScreenTurnover                     = 36
-	OffensiveFoulTurnover                     = 37
-	FiveSecondInboundTurnover                 = 38
-	StepOutOfBoundsTurnover                   = 39
-	OutOfBoundsLostBallTurnover               = 40
-	StealTurnover                             = 41
-	PlayerOutOfBoundsTurnover                 = 43
-	OutOfBoundsBadPassTurnover                = 45
+	TurnoverUnknown              TurnoverType = 0
+	TurnoverBadPass                           = 1
+	TurnoverLostBall                          = 2
+	TurnoverOutOfBounds                       = 3
+	TurnoverTraveling                         = 4
+	TurnoverFoul                              = 5
+	TurnoverDoubleDribble                     = 6
+	TurnoverDiscontinueDribble                = 7
+	TurnoverThreeSecondViolation              = 8
+	TurnoverFiveSecondViolation               = 9
+	TurnoverEightSecondViolation              = 10
+	TurnoverShotClock                         = 11
+	TurnoverInbound                           = 12
+	TurnoverBackcourt                         = 13
+	TurnoverOffensiveGoaltending              = 15
+	TurnoverLaneViolation                     = 17
+	TurnoverJumpBallViolation                 = 18
+	TurnoverKickedBall                        = 19
+	TurnoverIllegalAssist                     = 20
+	TurnoverPalming                           = 21
+	TurnoverPunchedBall                       = 33
+	TurnoverSwiningElbows                     = 34
+	TurnoverBasketFromBelow                   = 35
+	TurnoverIllegalScreen                     = 36
+	TurnoverOffensiveFoul                     = 37
+	TurnoverFiveSecondInbound                 = 38
+	TurnoverStepOutOfBounds                   = 39
+	TurnoverOutOfBoundsLostBall               = 40
+	TurnoverSteal                             = 41
+	TurnoverPlayerOutOfBounds                 = 43
+	TurnoverOutOfBoundsBadPass                = 45
 )
