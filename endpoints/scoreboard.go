5e6329ec734ceb1472e777eb4fa1caac5b9830db
Jackson Owens <jackson@chain.com>
2016-02-21 18:30:12 -0800

    Add LineScoreboardRow

--- endpoints/scoreboard.go
+++ endpoints/scoreboard.go
@@ -13,12 +13,47 @@ type ScoreboardParams struct {
 // ScoreboardResponse is the type for all result sets returned by the
 // 'scoreboardV2' resource.
 type ScoreboardResponse struct {
-	GameHeader  []*GameHeaderRow  `nbagame:"GameHeader"`
-	LineScore   []*LineScoreRow   `nbagame:"LineScore"`
-	LastMeeting []*LastMeetingRow `nbagame:"LastMeeting"`
+	GameHeader  []*GameHeaderRow     `nbagame:"GameHeader"`
+	LineScore   []*LineScoreboardRow `nbagame:"LineScore"`
+	LastMeeting []*LastMeetingRow    `nbagame:"LastMeeting"`
 
 	// TODO(jackson): Add support for EastConfStandingsByDay and
 	// WestConfStandingsByDay result sets.
+}
+
+// LineScoreboardRow represents the schema returned for 'LineScore' result
+// sets, returned from the 'scoreboardv2' resource. It contains slightly more
+// information than the 'LineScore' result set for the box score summary
+// endpoint.
+type LineScoreboardRow struct {
+	GameDateEST          string  `nbagame:"GAME_DATE_EST"`
+	GameSequence         int     `nbagame:"GAME_SEQUENCE"`
+	GameID               string  `nbagame:"GAME_ID"`
+	TeamID               int     `nbagame:"TEAM_ID"`
+	TeamAbbreviation     string  `nbagame:"TEAM_ABBREVIATION"`
+	TeamCityName         string  `nbagame:"TEAM_CITY_NAME"`
+	TeamWinsLosses       string  `nbagame:"TEAM_WINS_LOSSES"`
+	Q1                   int     `nbagame:"PTS_QTR1"`
+	Q2                   int     `nbagame:"PTS_QTR2"`
+	Q3                   int     `nbagame:"PTS_QTR3"`
+	Q4                   int     `nbagame:"PTS_QTR4"`
+	OT1                  int     `nbagame:"PTS_OT1"`
+	OT2                  int     `nbagame:"PTS_OT2"`
+	OT3                  int     `nbagame:"PTS_OT3"`
+	OT4                  int     `nbagame:"PTS_OT4"`
+	OT5                  int     `nbagame:"PTS_OT5"`
+	OT6                  int     `nbagame:"PTS_OT6"`
+	OT7                  int     `nbagame:"PTS_OT7"`
+	OT8                  int     `nbagame:"PTS_OT8"`
+	OT9                  int     `nbagame:"PTS_OT9"`
+	OT10                 int     `nbagame:"PTS_OT10"`
+	Total                int     `nbagame:"PTS"`
+	FieldGoalPercentage  float64 `nbagame:"PG_PCT"`
+	FreeThrowPercentage  float64 `nbagame:"PT_PCT"`
+	ThreePointPercentage float64 `nbagame:"FG3_PCT"`
+	Assists              int     `nbagame:"AST"`
+	Rebounds             int     `nbagame:"REB"`
+	Turnovers            int     `nbagame:"TOV"`
 }
 
 // ToData converts a ScoreboardResponse to a slice of data.Games.
