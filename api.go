fb44871acb7e8de3094f21b51ae23c1836628784
Jackson Owens <jackson@chain.com>
2016-02-21 23:36:07 -0800

    Implement API.Games.All()

--- api.go
+++ api.go
@@ -10,6 +10,15 @@ import (
 var (
 	// API is the default APIClient with default parameters.
 	API APIClient
+
+	// teamIDs is a slice of all the current NBA teams' team IDs.
+	teamIDs = []int{
+		1610612737, 1610612738, 1610612739, 1610612740, 1610612741, 1610612742,
+		1610612743, 1610612744, 1610612745, 1610612746, 1610612747, 1610612748,
+		1610612749, 1610612750, 1610612751, 1610612752, 1610612753, 1610612754,
+		1610612755, 1610612756, 1610612757, 1610612758, 1610612759, 1610612760,
+		1610612761, 1610612762, 1610612763, 1610612764, 1610612765, 1610612766,
+	}
 )
 
 func init() {
@@ -146,6 +155,20 @@ func (c *Players) Shots(playerID int) ([]*data.Shot, error) {
 
 type Games struct {
 	client *APIClient
+}
+
+// All returns the game IDs of all of the games played in the season.
+func (c *Games) All() ([]data.GameID, error) {
+	var gameIDs []data.GameID
+	for _, teamID := range teamIDs {
+		teamGameIDs, err := c.PlayedBy(teamID)
+		if err != nil {
+			return nil, err
+		}
+
+		gameIDs = append(gameIDs, teamGameIDs...)
+	}
+	return gameIDs, nil
 }
 
 // Details returns detailed information about the given game.
