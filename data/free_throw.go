e2960441bb179d4656aa04f40f8a83267520c279
Jackson Owens <jackson@chain.com>
2016-02-27 15:06:03 -0800

    Add free throw types

---
+++ data/free_throw.go
@@ -0,0 +1,19 @@
+package data
+
+// FreeThrowType defines the type of free throw.
+type FreeThrowType int
+
+const (
+	FreeThrowOneOfOne          = 10
+	FreeThrowOneOfTwo          = 11
+	FreeThrowTwoOfTwo          = 12
+	FreeThrowOneOfThree        = 13
+	FreeThrowTwoOfThree        = 14
+	FreeThrowThreeOfThree      = 15
+	FreeThrowTechnical         = 16
+	FreeThrowFlagrantOneOfTwo  = 18
+	FreeThrowFlagrantTwoOfTwo  = 19
+	FreeThrowFlagrantOneOfOne  = 20
+	FreeThrowClearPathOneOfTwo = 25
+	FreeThrowClearPathTwoOfTwo = 26
+)
