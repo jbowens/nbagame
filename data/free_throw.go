026b35be5a87ebd211291fe7d9cd55766bfc6268
Jackson Owens <jackson@chain.com>
2016-02-27 15:25:08 -0800

    Add free throw types

--- data/free_throw.go
+++ data/free_throw.go
@@ -1,5 +1,7 @@
 package data
 
+import "strings"
+
 // FreeThrowType defines the type of free throw.
 type FreeThrowType int
 
@@ -16,4 +18,29 @@ const (
 	FreeThrowFlagrantOneOfOne  = 20
 	FreeThrowClearPathOneOfTwo = 25
 	FreeThrowClearPathTwoOfTwo = 26
+)
+
+func (ft FreeThrowType) String() string {
+	return freeThrowToString[ft]
+}
+
+func (ft FreeThrowType) MarshalText() ([]byte, error) {
+	return []byte(strings.Replace(ft.String(), " ", "_", -1)), nil
+}
+
+var (
+	freeThrowToString = map[FreeThrowType]string{
+		FreeThrowOneOfOne:          "one of one",
+		FreeThrowOneOfTwo:          "one of two",
+		FreeThrowTwoOfTwo:          "two of two",
+		FreeThrowOneOfThree:        "one of three",
+		FreeThrowTwoOfThree:        "two of three",
+		FreeThrowThreeOfThree:      "three of three",
+		FreeThrowTechnical:         "technical",
+		FreeThrowFlagrantOneOfTwo:  "flagrant one of two",
+		FreeThrowFlagrantTwoOfTwo:  "flagrant two of two",
+		FreeThrowFlagrantOneOfOne:  "flagrant one of one",
+		FreeThrowClearPathOneOfTwo: "clear path one of two",
+		FreeThrowClearPathTwoOfTwo: "clear path two of two",
+	}
 )
