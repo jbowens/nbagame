6b6e0e3ca1ab32f8de59bd3ba00f9452bc439dbd
Jackson Owens <jackson@chain.com>
2016-02-27 14:20:15 -0800

    remove the defunct shots table

---
+++ db/migrations/20160227141900_RemoveShots.sql
@@ -0,0 +1,30 @@
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+DROP TABLE `shots`;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+CREATE TABLE `shots` (
+    id                          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id                     VARCHAR(20) NOT NULL,
+    player_id                   INT NOT NULL,
+    shot_number                 SMALLINT NOT NULL,
+    made                        TINYINT(1) NOT NULL,
+    points                      TINYINT(4) NOT NULL,
+    home                        TINYINT(1) NOT NULL,
+    period                      TINYINT(4) NOT NULL,
+    game_clock                  SMALLINT NOT NULL,
+    shot_clock                  DOUBLE NOT NULL,
+    dribbles                    SMALLINT NOT NULL,
+    touch_time_seconds          DOUBLE NOT NULL,
+    distance                    DOUBLE NOT NULL,
+    points_type                 TINYINT(4) NOT NULL,
+    closest_defender_player_id  INT NOT NULL,
+    closest_defender_distance   DOUBLE NOT NULL,
+    shot_type                   VARCHAR(50) NOT NULL,
+    description                 VARCHAR(50) NOT NULL,
+    zone                        VARCHAR(50) NOT NULL,
+    location_x                  SMALLINT NOT NULL,
+    location_y                  SMALLINT NOT NULL,
+    UNIQUE(game_id, player_id, shot_number)
+);
