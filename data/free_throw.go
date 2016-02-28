89ee9f45a5b7e457fc7825885eb691e295fc5e79
Jackson Owens <jackson@chain.com>
2016-02-27 23:10:06 -0800

    fix FreeThrowType constants

--- data/free_throw.go
+++ data/free_throw.go
@@ -9,18 +9,18 @@ import (
 type FreeThrowType int
 
 const (
-	FreeThrowOneOfOne          = 10
-	FreeThrowOneOfTwo          = 11
-	FreeThrowTwoOfTwo          = 12
-	FreeThrowOneOfThree        = 13
-	FreeThrowTwoOfThree        = 14
-	FreeThrowThreeOfThree      = 15
-	FreeThrowTechnical         = 16
-	FreeThrowFlagrantOneOfTwo  = 18
-	FreeThrowFlagrantTwoOfTwo  = 19
-	FreeThrowFlagrantOneOfOne  = 20
-	FreeThrowClearPathOneOfTwo = 25
-	FreeThrowClearPathTwoOfTwo = 26
+	FreeThrowOneOfOne          FreeThrowType = 10
+	FreeThrowOneOfTwo                        = 11
+	FreeThrowTwoOfTwo                        = 12
+	FreeThrowOneOfThree                      = 13
+	FreeThrowTwoOfThree                      = 14
+	FreeThrowThreeOfThree                    = 15
+	FreeThrowTechnical                       = 16
+	FreeThrowFlagrantOneOfTwo                = 18
+	FreeThrowFlagrantTwoOfTwo                = 19
+	FreeThrowFlagrantOneOfOne                = 20
+	FreeThrowClearPathOneOfTwo               = 25
+	FreeThrowClearPathTwoOfTwo               = 26
 )
 
 func (ft FreeThrowType) String() string {
